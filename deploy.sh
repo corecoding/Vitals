d997b5c7648e59287701a8511440bc46f898190c
Chris Monahan <chris@corecoding.com>
2018-06-25 07:22:42 -0700

    cleanup

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-NAME=freon@UshakovVasilii_Github.yahoo.com
-rm -rf ~/.local/share/gnome-shell/extensions/$NAME
-cp -r $NAME ~/.local/share/gnome-shell/extensions/.
--- makeZip.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-NAME=freon@UshakovVasilii_Github.yahoo.com
-cd $NAME
-zip -r $NAME.zip *
-cd ..
-mv $NAME/$NAME.zip .
-
