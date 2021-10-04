37c2810d629e7cc33ffce24537c8459a16284faa
Chris Monahan <chris@corecoding.com>
2021-10-04 07:23:43 -0400

    adds zip.sh

---
+++ zip.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+FILE=vitals.zip
+if [ -f $FILE ]; then
+    rm $FILE
+fi
+
+zip vitals.zip -r * -x "*.po" -x "locale/vitals.pot" -x "build.sh" -x "schemas/org.gnome.shell.extensions.vitals.gschema.xml"
