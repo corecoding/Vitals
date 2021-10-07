bedc1f9b763bb95df91b34de580c09eff30f3b0e
Chris Monahan <chris@corecoding.com>
2021-10-07 08:32:56 -0400

    add code to compile localization

--- zip.sh
+++ zip.sh
@@ -11,6 +11,24 @@ if [ -d ./schemas/ ]; then
     fi
 fi
 
+# check if msgfmt is missing
+if [ ! -x /usr/bin/msgfmt ]; then
+    # msgfmt doesn't exist, see if apt exists
+    if [ -x /usr/bin/apt ]; then
+        # install msgfmt command
+        sudo apt install gettext
+    fi
+fi
+
+# check if msgfmt exists
+if [ -x /usr/bin/msgfmt ]; then
+    if [ -d ./locale/ ]; then
+        for i in locale/*
+            do /usr/bin/msgfmt $i/LC_MESSAGES/vitals.po -o $i/LC_MESSAGES/vitals.mo
+        done
+    fi
+fi
+
 # zip up files, skipping blocked files as seen at
 # https://wiki.gnome.org/Projects/GnomeShell/Extensions/Review#Don.27t_include_unecessary_files
 zip vitals.zip -r * -x "locale/*.po" -x "locale/vitals.pot" -x "build.sh" -x "schemas/org.gnome.shell.extensions.vitals.gschema.xml"
