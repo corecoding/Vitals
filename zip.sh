e4dba45e9de08b06f4fbc06d4be6a59e33017288
Chris Monahan <chris@corecoding.com>
2021-10-07 08:23:54 -0400

    compile schemas commands

--- zip.sh
+++ zip.sh
@@ -4,4 +4,11 @@ if [ -f $FILE ]; then
     rm $FILE
 fi
 
+# check if schemas directory exists
+if [ -d ./schemas/ ]; then
+    if [ -x /usr/bin/glib-compile-schemas ]; then
+        glib-compile-schemas --strict schemas/
+    fi
+fi
+
 zip vitals.zip -r * -x "locale/*.po" -x "locale/vitals.pot" -x "build.sh" -x "schemas/org.gnome.shell.extensions.vitals.gschema.xml"
