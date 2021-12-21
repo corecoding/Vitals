ab564d341ec0b1bf27750323fbf7ff92c56ba6ca
Chris Monahan <chris@corecoding.com>
2021-12-21 09:23:57 -0500

    grab right helper file

--- sensors.js
+++ sensors.js
@@ -26,7 +26,7 @@
 
 const GObject = imports.gi.GObject;
 const Me = imports.misc.extensionUtils.getCurrentExtension();
-const FileModule = Me.imports.files;
+const FileModule = Me.imports.helpers.file;
 const Gettext = imports.gettext.domain(Me.metadata['gettext-domain']);
 const _ = Gettext.gettext;
 const NM = imports.gi.NM;
