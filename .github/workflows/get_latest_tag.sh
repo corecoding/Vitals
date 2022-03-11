68022050af6b89e5b3027151703786b7d7cad293
Chris Monahan <chris@corecoding.com>
2022-03-11 09:39:39 -0500

    debugging

--- .github/workflows/get_latest_tag.sh
+++ .github/workflows/get_latest_tag.sh
@@ -2,21 +2,24 @@
 
 tag_context=${TAG_CONTEXT:-repo}
 case "$tag_context" in
-    *repo*) 
+    *repo*)
+        echo "here 1"
         taglist="$(git for-each-ref --sort=-v:refname --format '%(refname:lstrip=2)' | grep -E "$tagFmt")"
         tag="$(semver $taglist | tail -n 1)"
 
         pre_taglist="$(git for-each-ref --sort=-v:refname --format '%(refname:lstrip=2)' | grep -E "$preTagFmt")"
         pre_tag="$(semver "$pre_taglist" | tail -n 1)"
         ;;
-    *branch*) 
+    *branch*)
+        echo "here 2"
         taglist="$(git tag --list --merged HEAD --sort=-v:refname | grep -E "$tagFmt")"
         tag="$(semver $taglist | tail -n 1)"
 
         pre_taglist="$(git tag --list --merged HEAD --sort=-v:refname | grep -E "$preTagFmt")"
         pre_tag=$(semver "$pre_taglist" | tail -n 1)
         ;;
-    * ) echo "Unrecognised context"; exit 1;;
+    * ) echo "Unrecognized context"; exit 1;;
 esac
 
-echo $tag
+echo *$tag*
+echo *$pre_tag*
