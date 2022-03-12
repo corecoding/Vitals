9e04d35bb51ec5dc96a10818edd9088d52cc4625
Chris Monahan <chris@corecoding.com>
2022-03-11 21:23:45 -0500

    try export

--- .github/workflows/get_latest_tag.sh
+++ .github/workflows/get_latest_tag.sh
@@ -6,11 +6,11 @@ tag_context=${TAG_CONTEXT:-repo}
 case "$tag_context" in
     *repo*)
         echo "here 1"
-        taglist="$(git for-each-ref --sort=-v:refname --format '%(refname:lstrip=2)' | grep -E "$tagFmt")"
-        tag="$(semver $taglist | tail -n 1)"
+        export taglist="$(git for-each-ref --sort=-v:refname --format '%(refname:lstrip=2)' | grep -E "$tagFmt")"
+        export tag="$(semver $taglist | tail -n 1)"
 
-        pre_taglist="$(git for-each-ref --sort=-v:refname --format '%(refname:lstrip=2)' | grep -E "$preTagFmt")"
-        pre_tag="$(semver "$pre_taglist" | tail -n 1)"
+        export pre_taglist="$(git for-each-ref --sort=-v:refname --format '%(refname:lstrip=2)' | grep -E "$preTagFmt")"
+        export pre_tag="$(semver "$pre_taglist" | tail -n 1)"
         ;;
     *branch*)
         echo "here 2"
