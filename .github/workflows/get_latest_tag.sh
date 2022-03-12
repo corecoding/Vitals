e9f6e845a00a2e1e00bd529b80acadbe9ecca0a2
Chris Monahan <chris@corecoding.com>
2022-03-12 08:40:40 -0500

    indent and echo

--- .github/workflows/get_latest_tag.sh
+++ .github/workflows/get_latest_tag.sh
@@ -8,8 +8,9 @@ case "$tag_context" in
     *repo*)
         echo "here 1"
         taglist="$(git for-each-ref --sort=-v:refname --format '%(refname:lstrip=2)' | grep -E "$tagFmt")"
-tag="$(semver $taglist | tail -n 1)"
         echo $taglist
+        tag="$(semver $taglist | tail -n 1)"
+        echo $tag
 
         pre_taglist="$(git for-each-ref --sort=-v:refname --format '%(refname:lstrip=2)' | grep -E "$preTagFmt")"
         pre_tag="$(semver "$pre_taglist" | tail -n 1)"
