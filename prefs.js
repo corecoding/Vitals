9dea5fa211801b1344514d505ad77ef27ea2a05e
Chris Monahan <chris@corecoding.com>
2023-03-11 08:26:04 -0500

    brings back nvidia code

--- prefs.js
+++ prefs.js
@@ -49,7 +49,7 @@ const Settings = new GObject.Class({
                         'show-network', 'show-storage', 'use-higher-precision',
                         'alphabetize', 'hide-zeros', 'include-public-ip',
                         'show-battery', 'fixed-widths', 'hide-icons',
-                        'include-static-info' ];
+                        'include-static-info', 'query-nvidia-smi' ];
 
         for (let key in sensors) {
             let sensor = sensors[key];
